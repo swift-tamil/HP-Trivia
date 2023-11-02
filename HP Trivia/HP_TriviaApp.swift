0d43308e1afed0247af14ca70a65ee64ff569fbb
MohamedGoush <0x1110111@gmail.com>
2023-11-02 07:46:37 +0530

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by python on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
